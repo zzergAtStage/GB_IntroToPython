83beb72509c76fd0fcda7ab1526f238f427e7994
Sergio Brü <brusentsovsv@gmail.com>
2023-05-23 01:09:29 +0200

    s07 second issue

--- S07/task038.py
+++ S07/task038.py
@@ -21,9 +21,6 @@
 #multiplication table
 
 def print_operation_table(res,num_rows=6, num_columns=6):
-    for i in range(1,num_rows):
-        for y in range(1,num_columns):
-            print(str(res(i,y)))
-
+    print('\n'.join(['\t'.join([str(res(i,y))for y in range(1,num_columns + 1)])for i in range(1,num_rows + 1)]))
 res = lambda x,y: x*y
-print_operation_table(res,3,3)    
+print_operation_table(res,6,6)    
